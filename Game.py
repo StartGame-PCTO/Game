8888e16e259e63e2e68fe297913e410822db3d9f
Davide De Carlini <90908058+davide12deca@users.noreply.github.com>
2022-02-03 13:33:09 +0100

    Add files via upload

--- Game.py
+++ Game.py
@@ -1,17 +1,79 @@
 import cv2
 import numpy as np
 import pygame
+import math
+TEN = 10
+
+def convertiX(x):
+    return math.floor(x/TEN)
+
+def convertiY(y):
+    return math.floor(y/TEN)
 
 def colorDetec():
-    DIMENSIONI = (600,600)
+    level = [
+    "RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo",
+    "++++++++++++++++++++++++++++++RRoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo",
+    "++++++++++++++++++++++++++++++RRoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo",
+    "++++++++++++++++++++++++++++++RRooooooooooooooooooooooooooooooRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRoooo",
+    "++++++++++++++++++++++++++++++RRooooooooooooooooooooooooooooooRR++++++++++++++++++++++++++++++RRoooo",
+    "++++++++++++++++++++++++++++++RRooooooooooooooooooooooooooooooRR++++++++++++++++++++++++++++++RRoooo",
+    "RRRRRRRRRRRRRRRRRRRR++++++++++RRooooooooooooooooooooooooooooooRR++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++RRooooooooooooooooooooooooooooooRR++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++RRooooooooooooooooooooooooooooooRR++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++RRooRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR++++++++++RRRRRRRRRR++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++RRooRR++++++++++++++++++++++++++++++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++RRooRR++++++++++++++++++++++++++++++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++RRooRR++++++++++++++++++++++++++++++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++RRooRR++++++++++++++++++++++++++++++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++RRooRR++++++++++++++++++++++++++++++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++RRRRRR++++++++++RRRRRRRRRRRRRRRRRR++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++++++++++++++++++RRooooooooooooooRR++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++++++++++++++++++RRooooooooooooooRR++++++++++RRRRRRRRRR++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++++++++++++++++++RRooooooooooooooRR++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++++++++++++++++++RRooooooooooooooRR++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooooooRR++++++++++++++++++++++++++RRooooooooooooooRR++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooooooRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRooooooooooooooRR++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooRR++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooRR++++++++++RRRRRRRRRRRRRRRRRRRRRRoooo",
+    "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooRR++++++++++RRoooooooooooooooooooooooo",
+    "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooRR++++++++++RRoooooooooooooooooooooooo",
+    "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooRR++++++++++RRoooooooooooooooooooooooo",
+    "ooooooooooooooRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR++++++++++RRoooooooooooooooooooooooo",
+    "ooooooooooooooRR++++++++++++++++++++++++++++++++++++++++++++++++++++++++++RRRRRRRRRRRRRRRRRRRRRRoooo",
+    "ooooooooooooooRR++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooRR++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooRR++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooRR++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++RRoooo",
+    "ooooooooooooooRR++++++++++RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR++++++++++RRRRRRRRRR++++++++++RRoooo",
+    "ooooooooooooooRR++++++++++RRooooooooooooooooooooooooooooooooooRR++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooRR++++++++++RRooooooooooooooooooooooooooooooooooRR++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooRR++++++++++RRooooooooooooooooooooooooooooooooooRR++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooRR++++++++++RRooooooooooooooooooooooooooooooooooRR++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooRR++++++++++RRooooooooooooooooooooooooooooooooooRR++++++++++RRooooooRR++++++++++RRoooo",
+    "ooooooooooooooRRRRRRRRRRRRRRooooooooooooooooooooooooooooooooooRRRRRRRRRRRRRRooooooRR++++++++++RRoooo",
+    "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooRR++++++++++RRoooo",
+    "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooRR++++++++++RRoooo",
+    "RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR++++++++++RRoooo",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++RRoooo",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++RRoooo",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++RRoooo",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++RRoooo",
+    "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++RRoooo",
+    "RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRoooo",
+    "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo",
+]
+    DIMENSIONI = (len(level[1])*TEN,len(level)*TEN)
     pygame.init()
     screen = pygame.display.set_mode(DIMENSIONI)
+    bg = pygame.image.load("lab01.jpeg")
     pygame.display.set_caption('Prova')
-    colore = (255,0,0)
-    nero = (0,0,0)
-    state = True
-    centroX = 300
-    centroY = 300
+    pallina = pygame.image.load("Pallina.png")
+
+
+    centroX = 10
+    centroY = 30
+
     kernel = np.ones((8 ,8), np.uint8)
 
     def nothing(x):
@@ -68,18 +130,23 @@ def colorDetec():
             break
 
         print(x,y)
-        screen.fill(colore)
-        pygame.draw.circle(screen,nero,(centroX,centroY),10)
+        screen.blit(bg, (0,0))
+        screen.blit(pallina,(centroX,centroY))
         if x > 240 and x < 360 and y > 200 and y < 280:
             print("centro, fermo")
         if x > 240 and x < 360 and y > 0 and y < 200:
-            centroY -= 1
+            if level[convertiY(centroY-1)][convertiX(centroX)] != "R":
+                centroY -= 1
         if x > 240 and x < 360 and y > 280 and y < 480:
-            centroY += 1
+            if level[convertiY(centroY+1)][convertiX(centroX)] != "R":
+                centroY += 1
         if x > 0 and x < 240 and y > 200 and y < 280:
-            centroX -= 1
+            if level[convertiY(centroY)][convertiX(centroX-1)] != "R":
+                centroX -= 1
         if x > 360 and x < 640 and y > 200 and y < 280:
-            centroX += 1
+            if level[convertiY(centroY)][convertiX(centroX+1)] != "R":
+                centroX += 1
+
         pygame.display.update()
 
 def main():
